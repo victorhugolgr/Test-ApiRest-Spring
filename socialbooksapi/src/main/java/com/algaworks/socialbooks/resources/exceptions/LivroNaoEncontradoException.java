35f046e5e389bc42736c1d730a541021340f912f
Victor Hugo <victorhugolgr@gmail.com>
2016-10-24 23:17:32 -0300

    project

---
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/exceptions/LivroNaoEncontradoException.java
@@ -0,0 +1,20 @@
+package com.algaworks.socialbooks.resources.exceptions;
+
+public class LivroNaoEncontradoException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public LivroNaoEncontradoException(String mensagem) {
+		super(mensagem);
+	}
+
+	public LivroNaoEncontradoException(String mensagem, Throwable causa) {
+		super(mensagem,causa);
+	}
+	
+	
+
+}
