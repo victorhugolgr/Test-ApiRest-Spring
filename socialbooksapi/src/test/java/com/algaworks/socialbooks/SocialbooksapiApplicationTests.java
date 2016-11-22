58d1a711d469c573a3d8561b33b1d138688fd368
Victor Hugo <victorhugolgr@gmail.com>
2016-11-21 22:19:51 -0300

    Teste unitário.

--- socialbooksapi/src/test/java/com/algaworks/socialbooks/SocialbooksapiApplicationTests.java
+++ socialbooksapi/src/test/java/com/algaworks/socialbooks/SocialbooksapiApplicationTests.java
@@ -1,16 +1,27 @@
 package com.algaworks.socialbooks;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.algaworks.socialbooks.domain.Livro;
+import com.algaworks.socialbooks.services.LivrosService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SocialbooksapiApplicationTests {
 
+	@Autowired
+	private LivrosService livrosService;
+	
 	@Test
 	public void contextLoads() {
+		Livro livro = livrosService.salvar(new Livro("GIT Passo-a-passo"));
+		assertNotNull(livro.getId());
 	}
 
 }
